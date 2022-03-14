9f5b34d702c14f3181ddc7267383de9624f1f63d
Adam Szummer <adam.szummer@digica.com>
2022-03-14 20:27:26 +0100

    reading gt, adding noise, creating iou per frame

---
+++ main.py
@@ -0,0 +1,188 @@
+from multiprocessing.connection import wait
+import utils,os
+import cv2
+from argparse import ArgumentParser
+import numpy as np
+from evaluation import voc_eval
+import matplotlib.pyplot as plt
+from utils import generate_noisy_bboxes
+
+ANNOTATIONS = 'Lab1/ai_challenge_s03_c010-full_annotation.xml'
+DATA_FRAMES = 'data'
+VIDEO_PATH = '/home/aszummer/Documents/MCV/M6/Team3-Project/Lab1/AICity_data/AICity_data/train/S03/c010/vdo.avi'
+GT_COLOR = (0, 255, 0)
+PREDICTED_COLOR = (255, 0, 0)
+PREDICTIONS_MODELS = {
+    'mask_rcnn': 'det_mask_rcnn.txt',
+    'ssd512': 'det_ssd512.txt',
+    'yolov3': 'det_yolo3.txt'
+}
+EXPERIMENTS_FOLDER = "experiments"
+# python main.py -v /home/sergio/MCV/M6/data/AICity_data/train/S03/c010/vdo.avi -a /home/sergio/MCV/M6/data/ai_challenge_s03_c010-full_annotation.xml
+
+display = True
+
+def show_boxes(input_video, gt_rect, predictions):
+
+    cap = cv2.VideoCapture(input_video)
+    frame_n = 0
+    wait_time = 1
+    gt_frame_keys, pred_frame_keys = list(gt_rect.keys()), list(predictions.keys())
+
+    while(True):
+        ret, img = cap.read()
+        if frame_n in gt_frame_keys:
+            for r in gt_rect[frame_n]:
+                img = cv2.rectangle(img, (int(r[0]), int(r[1])),  (int(r[2]), int(r[3])), GT_COLOR, 2)
+        
+        if frame_n in pred_frame_keys:
+            for r in predictions[frame_n]:
+                img = cv2.rectangle(img, (int(r[0]), int(r[1])),  (int(r[2]), int(r[3])), PREDICTED_COLOR, 2)
+        
+        if not ret: # End of video sequence
+            break
+        
+        if display:
+            display_frame = cv2.resize(img, tuple(np.int0(0.5*np.array(img.shape[:2][::-1]))))
+            cv2.imshow('frame',display_frame)
+            k = cv2.waitKey(wait_time)
+            if k == ord('q'):
+                break
+            elif k == ord('p'):
+                wait_time = int(not(bool(wait_time)))
+        frame_n+=1
+    
+    cap.release()
+    cv2.destroyAllWindows()
+
+def read_annotations(annotations, predictions_folder):
+    # gt_rect format: 
+    # {frame_number: [[x_min1, y_min1, x_max1, y_max1], [x_min2, y_min2, x_max2, y_max2], [], ...], ...}
+    print("Reading annotations from:", annotations)
+    gt_rect = utils.parse_xml_reacts(annotations)
+    
+    # pred_rect format: 
+    # { "rcnn": {frame_number: [[x_min1, y_min1, x_max1, y_max1], [x_min2, y_min2, x_max2, y_max2], [], ...], ...},
+    #   "yolo": {frame_number: [[x_min1, y_min1, x_max1, y_max1], [x_min2, y_min2, x_max2, y_max2], [], ...], ...},
+    # }
+    print("Reading predictions from:", predictions_folder)
+    predictions = {model_name: utils.parse_predictions_rects(os.path.join(predictions_folder, PREDICTIONS_MODELS[model_name])) for model_name in list(PREDICTIONS_MODELS.keys())}
+    
+    return gt_rect, predictions
+
+def show_iou_over_time_and_compute(video_path, gt, pred, run_name, save=True):
+    print("Computing mIoU")
+    
+    if not os.path.exists(EXPERIMENTS_FOLDER):
+        os.mkdir(EXPERIMENTS_FOLDER)
+    if not os.path.exists(EXPERIMENTS_FOLDER+"/"+str(run_name)):
+        os.mkdir(EXPERIMENTS_FOLDER+"/"+str(run_name))
+    
+    mIoU = []
+    save = False
+
+    cap = cv2.VideoCapture(video_path)
+    frame_num = 0
+    ret, img = cap.read()
+    total_frames = len(list(gt.keys()))
+
+    while(True):
+        gt_bboxes = gt[frame_num]
+        det_bboxes = pred[frame_num]
+        mIoU.append(utils.get_frame_iou(gt_bboxes, det_bboxes))
+
+        #display_frame = cv2.resize(img, tuple(np.int0(0.5*np.array(img.shape[:2][::-1]))))
+        
+        for r in gt_bboxes:
+            img = cv2.rectangle(img, (int(r[0]), int(r[1])),  (int(r[2]), int(r[3])), GT_COLOR, 2)
+    
+        for r in det_bboxes:
+            img = cv2.rectangle(img, (int(r[0]), int(r[1])),  (int(r[2]), int(r[3])), PREDICTED_COLOR, 2)
+        
+        if save:
+            plt.plot(mIoU, c="black")
+            plt.xlabel('Frame number')
+            plt.ylabel('Mean IoU')
+            plt.xlim([0, total_frames])
+            plt.ylim([0, 1])
+            plt.title("Mean IoU over time")
+            plt.tight_layout()
+            plt.savefig(EXPERIMENTS_FOLDER+'/'+str(run_name)+'/iou_plt_'+str(frame_num)+'.png')
+        
+        ret, img = cap.read()
+        frame_num += 1
+        
+        if not ret:
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+    # Compute mean IoU over time
+    print('Mean IoU over time for',run_name,':',np.mean(mIoU))
+
+    plt.figure()
+    plt.plot(mIoU, c="black")
+    plt.xlim([0, total_frames])
+    plt.ylim([0, 1])
+    plt.title("Mean IoU over time")
+    plt.xlabel('Frame number')
+    plt.ylabel('Mean IoU')
+    plt.title('Mean IoU over time')
+    plt.tight_layout()
+    plt.savefig(EXPERIMENTS_FOLDER+'/'+str(run_name)+'/iou_plt_final.png')
+
+def parse_arguments():
+    parser = ArgumentParser()
+    parser.add_argument("-v",
+                        dest="input_video",
+                        required=True,
+                        type=str,
+                        help="input_video")
+    parser.add_argument("-a",
+                        dest="annotations",
+                        required=True,
+                        type=str,
+                        help="annotations")
+    parser.add_argument("-s",
+                        dest="save_plot",
+                        required=True,
+                        default=False,
+                        type=bool,
+                        help="Save mIoU plot")
+    parser.add_argument("-r",
+                        dest="run_name",
+                        required=True,
+                        default="default",
+                        type=str,
+                        help="Name of experiment")
+    args = parser.parse_args()
+
+    return args.input_video, args.annotations, args.run_name, args.save_plot
+
+if __name__ == "__main__":
+    input_video, annotations, run_name, save_plot = parse_arguments()
+    gt, predictions = read_annotations(annotations, os.path.join("/".join(input_video.split("/")[:-1]), "det"))
+
+    # Show bboxes
+    # show_boxes(input_video, gt, predictions["yolov3"][0])
+    
+    # Compute mAP
+    # print("MaskRCNN mAP 0.5:", voc_eval(predictions["mask_rcnn"][1], gt, ovthresh=0.5))
+    # print("YoloV3 mAP 0.5:", voc_eval(predictions["yolov3"][1], gt, ovthresh=0.5))
+    # print("SSD512 mAP 0.5:", voc_eval(predictions["ssd512"][1], gt, ovthresh=0.5))
+    
+    # Evaluating noisy AP
+    rank = 10
+    frame_ids, tot_boxes, confidences = generate_noisy_bboxes(gt, prob_drop_detections=0, std_coords=10, resizing_factor=1, rank=rank)
+    preds_formatted = utils.predictions_to_gt_format(frame_ids, tot_boxes)
+    print("Default reranking mAP:", np.mean(np.array([voc_eval([frame_ids, tot_boxes, confidences[i]], gt, ovthresh=0.5) for i in range(rank)])))
+    show_boxes(input_video, gt, preds_formatted)
+    
+    # Show AP over time ?
+    
+    # Show IoU over time + Compute mean IoU
+    show_iou_over_time_and_compute(input_video, gt, predictions["yolov3"][0], run_name+"_yolov3", save=save_plot)
+    show_iou_over_time_and_compute(input_video, gt, predictions["mask_rcnn"][0], run_name+"_maskrcnn", save=save_plot)
+    show_iou_over_time_and_compute(input_video, gt, predictions["ssd512"][0], run_name+"_ssd512", save=save_plot)
+    
---
+++ utils.py
@@ -0,0 +1,196 @@
+from bs4 import BeautifulSoup
+import numpy as np
+from tqdm import tqdm
+import pandas as pd
+import random
+import copy
+
+WIDTH = 1920
+HEIGHT = 1080
+
+
+def generate_noisy_bboxes(gt_bboxes, rank=10, std_coords=0, resizing_factor=1, prob_drop_detections=0, prob_new_detections=0 , dropout = 0):
+    """Generate noisy bounding boxes from groundtruth annotations.
+
+    Args:
+        gt_bboxes (Dict): GT annotations in format: 
+            {
+                'imagename': {
+                    'bbox': [['x1', 'y1', 'x2', 'y2'], ['x1', 'y1', 'x2', 'y2']],
+                    'difficult':[True, False],
+                    'det': [False, False]
+                }, ...
+            }
+        rank (int > 0): How many random rankings are generated 
+        std_coords (float): Standard deviation for Gaussian noise
+        resizing_factor (1 > float > 0): Resizing factor of the bounding box
+        prob_drop_detections (1 > float > 0): Probability of dropping bounding box in the frame
+        prob_new_detections (1 > float > 0): Probability of adding new bounding box in the frame (how to determine width/height)
+    
+    Return:
+        [np.ndarray of frame ids (number of frames,), np.ndarray of bounding box detections (number of frames, 4), list of confidences (rank, number of frames)]
+    """
+    gt_bboxes = copy.deepcopy(gt_bboxes)
+    tot_boxes = []
+    confidences = np.array([])
+    frame_ids = []
+    for frame_id in sorted(list(gt_bboxes.keys())):
+        frame_bboxes = gt_bboxes[frame_id]
+        # Modify frame_bboxes / add new ones / remove
+
+        if std_coords != 0:
+            noise = np.random.normal(0, std_coords, size=(len(frame_bboxes), 4))
+            for i in range(0, len(frame_bboxes)):
+                x0, y0, x1, y1 = frame_bboxes[i]
+                frame_bboxes[i] = [x0-noise[i,0], y0-noise[i,1], x1+noise[i,2], y1+noise[i,3]] 
+
+        #resize the bounding box by factor 0-1
+        if resizing_factor != 1:
+            for i in range(0,len(frame_bboxes)):
+                x0, y0, x1, y1 = frame_bboxes[i]
+                w, h = x1-x0, y1-y0
+                resized_item = np.asarray([x0-(w*(resizing_factor - 1)/2), y0-(h*(resizing_factor - 1)/2), x1+(w*(resizing_factor - 1)/2), y1+(h*(resizing_factor - 1)/2)])
+                resized_item = np.round(resized_item, decimals = 2)
+                frame_bboxes[i] = list(resized_item)
+
+        #adding new bounding box with prob_new_detection probability
+        if prob_new_detections != 0:
+            if random.random() < prob_new_detections:
+                x1 = random.randint(0,WIDTH)    
+                x2 = random.randint(x1,WIDTH)
+                y1 = random.randint(0,HEIGHT)
+                y2 = random.randint(y1,HEIGHT)
+                frame_bboxes.append([x1,y2,x2,y2])
+        
+        #deleting bounding box with prob_drop_detection probability
+        if prob_drop_detections != 0:
+            frame_bboxes = np.array(frame_bboxes)
+            dets_to_keep = np.random.random(len(frame_bboxes)) > prob_drop_detections
+            frame_bboxes = frame_bboxes[dets_to_keep]
+            frame_bboxes = frame_bboxes.tolist()
+
+        #deletes random index X times as defined by dropout amount, 0.5 deletes half of indexes
+        if dropout != 0:
+            boxes_to_delete = int(len(frame_bboxes)*dropout)
+            for i in range(boxes_to_delete):
+                frame_bboxes.pop(random.randint(0,len(frame_bboxes)-1))
+        
+        # Add bboxes, ids and confidences to total bboxes, ids and confidences
+        if len(frame_bboxes) > 0:
+            tot_boxes.append(np.array(frame_bboxes))
+            frame_ids = frame_ids + [frame_id]*len(frame_bboxes)
+    
+    frame_ids = np.array(frame_ids)
+    confidences = np.random.random((rank, len(frame_ids)))
+    tot_boxes = np.concatenate(tot_boxes, 0)
+    
+    # Return in required format for evaluation
+    return frame_ids, tot_boxes, confidences
+    
+    
+
+def parse_xml_reacts(path_to_anno):
+    
+    # Reading the data inside the xml
+    # file to a variable under the name
+    # data
+    with open(path_to_anno, 'r') as f:
+        data = f.read()
+    
+    # Passing the stored data inside
+    # the beautifulsoup parser, storing
+    # the returned object
+    Bs_data = BeautifulSoup(data, "xml")
+    
+    # Finding all instances of tag
+    # `unique`
+    tracks_all = Bs_data.find_all('track')
+    frame_dict = {}
+    for item in tqdm(tracks_all):
+        if item.get('label') == 'car':
+            boxs_in_track = item.find_all('box')
+            for box in boxs_in_track:
+                # print(box.get('frame'))
+                frame_n = int(box.get('frame')) 
+                if frame_n not in frame_dict:
+                    frame_dict[frame_n] = []
+                frame_dict[frame_n].append([float(box.get('xtl')), float(box.get('ytl')), 
+                    float(box.get('xbr')), float(box.get('ybr'))])
+    return frame_dict
+
+def parse_predictions_rects(path):
+    """
+    Input:
+        - Path to annotation txt where each row contains information about:
+            frame number, bounding_box_coords, confidence
+    Output format
+        dict[frame_num] = [[x1, y1, x2, y2]]
+    """
+    # COL_NAMES = ['frame', 'id', 'bb_left', 'bb_top', 'bb_width', 'bb_height', 'conf', 'x', 'y', 'z']
+    COL_NAMES = ['frame', 'bb_left', 'bb_top', 'bb_width', 'bb_height', 'conf']
+    
+    predictions_dict = {}
+    df = pd.read_csv(path, delimiter=',', usecols=[0, 2, 3, 4, 5, 6], names=COL_NAMES)
+    df.iloc[:,0] = df.iloc[:,0] - 1
+    df.iloc[:,3] = df.iloc[:,3] + df.iloc[:,1]
+    df.iloc[:,4] = df.iloc[:,4] + df.iloc[:,2]
+    
+    last_frame = 0
+    bboxes = []
+    for i in range(len(df)):
+        frame = df['frame'][i]
+        if last_frame != frame:
+            predictions_dict[last_frame] = bboxes
+            last_frame = frame
+            bboxes = []
+        
+        bbox_p_conf = df.iloc[i, 1:-1]
+        bboxes.append(list(bbox_p_conf.values))
+    
+    predictions_dict[frame] = bboxes
+    
+    bboxes = df[['bb_left', 'bb_top', 'bb_width', 'bb_height']].values
+    
+    return predictions_dict, [df['frame'].values, bboxes, df['conf'].values]
+    
+def predictions_to_gt_format(frame_ids, tot_boxes):
+    preds_formatted = {}
+    for i in np.unique(frame_ids):
+        preds_formatted[i] = tot_boxes[frame_ids == i]
+    return preds_formatted
+
+def get_rect_iou(a, b):
+    """Return iou for a single a pair of boxes"""
+    x11, y11, x12, y12 = a
+    x21, y21, x22, y22 = b
+
+    xA = max(x11,x21)
+    yA = max(y11,y21)
+    xB = min(x12,x22)
+    yB = min(y12,y22)
+     
+    # respective area of ​​the two boxes
+    boxAArea=(x12-x11)*(y12-y11)
+    boxBArea=(x22-x21)*(y22-y21)
+     
+     # overlap area
+    interArea=max(xB-xA,0)*max(yB-yA,0)
+     
+     # IOU
+    return interArea/(boxAArea+boxBArea-interArea)
+
+def get_frame_iou(gt_rects, det_rects):
+    """Return iou for a frame"""
+    list_iou = []
+
+    for gt in gt_rects:
+        max_iou = 0
+        for det in det_rects:
+            iou = get_rect_iou(det, gt)
+            if iou > max_iou:
+                max_iou = iou
+        
+        if max_iou != 0:
+            list_iou.append(max_iou)
+
+    return np.mean(list_iou)
