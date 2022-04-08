ba04fdd821537004792f079cc27579747a994f18
smontode24 <sergio.mp1710@gmail.com>
2022-04-08 19:22:39 +0200

    Visualization of annotations

--- week5/datasets.py
+++ week5/datasets.py
@@ -24,6 +24,74 @@ class AICityDataset(torch.utils.data.Dataset):
             for sequence in sequences:
                 dict_seqs[sequence] = sorted(os.listdir(os.path.join(path, sequence)))
             sequences = dict_seqs
+        
+        for sequence, camera_ids in sequences.items():
+            for camera_id in camera_ids:
+                cam_folder = os.path.join(path, sequence, camera_id)
+                if os.path.isdir(cam_folder):
+                    gt = pd.read_csv(os.path.join(cam_folder, 'gt','gt.txt'),
+                                    names=['frame', 'id', 'left', 'top', 'width', 'height',
+                                            '1','2','3','4'])  # extra useless cols
+                    cap = cv2.VideoCapture(os.path.join(cam_folder, 'vdo.avi'))
+                    # Load only frames that are labeled
+                    #frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+                    self.gts.append(gt)
+                    self.videos.append(cap)
+                    lengths.append(len(np.unique(gt["frame"].values)))
+        self.video_starts = np.array(lengths).cumsum()
+        self.length = sum(lengths)
+        self.lengths = lengths
+
+    def __getitem__(self, idx):
+        vid_idx = (self.video_starts[1:] >= idx).argmax()
+        #idx_list_all = idx - self.video_starts[vid_idx]
+        frame_id = self.gts[vid_idx].iloc[idx - self.video_starts[vid_idx], 0]
+        
+        self.videos[vid_idx].set(cv2.CAP_PROP_POS_FRAMES, frame_id)
+        ret, img = self.videos[vid_idx].read()
+        if self.transformations:
+            img = self.transformations(img[:,:,::-1].copy())
+
+        bboxes = self.gts[vid_idx][self.gts[vid_idx]['frame'] == frame_id][['left','top','width','height','id']].to_numpy()
+        bboxes[:,2] = bboxes[:,0] + bboxes[:,2]
+        bboxes[:,3] = bboxes[:,1] + bboxes[:,3]
+        labels = torch.ones((len(bboxes)), dtype=torch.int64)*self.class_car_number
+        target = {'boxes': torch.tensor(bboxes[:,:4], dtype=torch.float32), 'track_id':torch.tensor(bboxes[:,4], dtype=torch.float32), 'labels': labels, 'frame_id': torch.tensor([frame_id])}
+        return img, target
+        
+    def __len__(self):
+        return self.length
+    
+    def contains_gt_for_frame(self, frame_idx):
+        if len(self.lengths) == 2:
+            return len(self.gts[0][self.gts[0]['frame'] == frame_idx]) != 0
+        raise ValueError("Evaluating in more than one sequence")
+    
+    def get_bboxes_of_frame_id(self, frame_idx):
+        if len(self.lengths) == 2:
+            return self.gts[0][self.gts[0]['frame'] == frame_idx]
+        raise ValueError("Evaluating in more than one sequence")
+    
+    
+# AICityDatasetEval part
+class AICityDatasetValidation(torch.utils.data.Dataset):
+    def __init__(self, path, sequences, transformations=None):
+        """
+        sequences can be:
+            - list of strings: ['S01', 'S03']. This will load all videos from all cameras
+            - dict: {'S01':['c001', 'c002']}. So we can choose the cameras
+        """
+        self.transformations = transformations
+        self.path = path
+        self.videos = []
+        self.gts = []
+        self.class_car_number = 3
+        lengths = [0]
+        if isinstance(sequences, list):
+            dict_seqs = {}
+            for sequence in sequences:
+                dict_seqs[sequence] = sorted(os.listdir(os.path.join(path, sequence)))
+            sequences = dict_seqs
 
         for sequence, camera_ids in sequences.items():
             for camera_id in camera_ids:
@@ -37,7 +105,7 @@ class AICityDataset(torch.utils.data.Dataset):
                     #frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
                     self.gts.append(gt)
                     self.videos.append(cap)
-                    lengths.append(len(gt["frame"].values.unique()))
+                    lengths.append(len(np.unique(gt["frame"].values)))
         self.video_starts = np.array(lengths).cumsum()
         self.length = sum(lengths)
         self.lengths = lengths
---
+++ week5/visualize_gt.py
@@ -0,0 +1,105 @@
+import cv2
+import os
+#import time
+#from collections import defaultdict
+
+import numpy as np
+import cv2
+#import imageio
+
+from tkinter import E
+import cv2
+import numpy as np
+from argparse import ArgumentParser
+
+import scipy
+from utils import read_annotations, image_to_tensor
+#from evaluation import show_annotations_and_predictions, voc_eval
+import os
+from tqdm import tqdm
+from torchvision.ops import nms
+from datasets import AICityDataset
+import utils
+import sys
+
+import torch
+from models import load_model
+from sort import Sort
+from sort import convert_x_to_bbox
+import motmetrics as mm
+import random
+
+WAIT_TIME = 1
+SAVE = True
+CAR_LABEL_NUM = 3
+FRAME_25PER = 510
+EXPERIMENTS_FOLDER = "experiments"
+STORED_gt_boxesECTIONS_NAME = "gt_boxess.txt"
+SHOW_THR = 0.5
+RESULTS_FILENAME = "results"
+COLORS = [(int(random.random() * 256), int(random.random() * 256), int(random.random() * 256)) for i in range(10000)]
+
+def visualize_gt(video_path):
+    """
+    Object tracking: tracking by Kalman
+    3 parameters: gt_boxesection threshold, minimum iou to match track, and maximum frames to skip between tracked boxes.
+    """
+
+    # Prepare video capture
+    cap = cv2.VideoCapture(video_path)
+    last_frame = cap.get(cv2.CAP_PROP_FRAME_COUNT)
+    length = last_frame
+    cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
+    
+    ret = True
+    frame_number = 0
+    
+    ret, img = cap.read()
+
+    # Create dataset
+    dataset_path = os.path.dirname(os.path.dirname(os.path.dirname(video_path)))
+    sequences = {os.path.basename(os.path.dirname(os.path.dirname(video_path))):
+                 [os.path.basename(os.path.dirname(video_path))]}
+    dataset = AICityDataset(dataset_path, sequences)
+    #import pdb
+    #pdb.set_trace()
+    
+    with torch.no_grad():
+        with tqdm(total=length, file=sys.stdout) as pbar:
+            while ret:
+                img_draw = img.copy()
+                if dataset.contains_gt_for_frame(frame_number):
+                    gt = dataset.get_bboxes_of_frame_id(frame_number)
+                    for i_gt_box in range(len(gt)):
+                        gt_boxes = gt.iloc[i_gt_box, :]
+                        track_id = int(gt_boxes["id"])
+                        # 'left', 'top', 'width', 'height'
+                        img_draw = cv2.rectangle(img_draw, (int(gt_boxes["left"]), int(gt_boxes["top"])), (int(gt_boxes["left"]+gt_boxes["width"]), int(gt_boxes["top"]+gt_boxes["height"])), COLORS[track_id], 2)
+                        img_draw = cv2.rectangle(img_draw, (int(gt_boxes["left"]), int(gt_boxes["top"]-20)), (int(gt_boxes["left"]+gt_boxes["width"]), int(gt_boxes["top"])), COLORS[track_id], -2)
+                        img_draw = cv2.putText(img_draw, str(track_id), (int(gt_boxes["left"]), int(gt_boxes["top"])), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 0), 2)
+                        
+                cv2.imshow('Tracking results', cv2.resize(img_draw, (int(img_draw.shape[1]*0.5), int(img_draw.shape[0]*0.5))))
+                k = cv2.waitKey(1)
+                if k == ord('q'):
+                    return
+            
+                frame_number += 1
+                ret, img = cap.read()
+                pbar.update(1)
+
+    cv2.destroyAllWindows()
+
+def parse_arguments():
+    parser = ArgumentParser()
+    parser.add_argument("-v",
+                        dest="input_video",
+                        required=True,
+                        type=str,
+                        help="Input video for analyzing mIou/mAP")
+    args = parser.parse_args()
+    return args.input_video
+    
+if __name__ == "__main__":
+    input_video = parse_arguments()
+    visualize_gt(input_video)
+    
